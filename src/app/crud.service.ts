72f50456f1e9c12575331398596244296503992f
sagar <sagarsangale.b@gmail.com>
2024-05-27 17:42:35 +0530

    change URL

--- src/app/crud.service.ts
+++ src/app/crud.service.ts
@@ -10,15 +10,15 @@ export class CrudService {
   constructor(private http:HttpClient) { }
 
   addEmployee(data:any):Observable<any>{
-   return this.http.post("https://sagarangular.github.io/host_api/db.json",data)
+   return this.http.post("http://localhost:3000/employees",data)
   }
   getEmployee(){
-    return this.http.get("https://sagarangular.github.io/host_api/db.json")
+    return this.http.get("http://localhost:3000/employees")
   }
   deleteEmployee(id:any):Observable<any>{
-    return this.http.delete(`https://sagarangular.github.io/host_api/db.json/${id}`)
+    return this.http.delete(`http://localhost:3000/employees/${id}`)
   }
   updateEmployee(id:any,data:any):Observable<any>{
-    return this.http.put(`https://sagarangular.github.io/host_api/db.json/${id}`,data)
+    return this.http.put(`http://localhost:3000/employees/${id}`,data)
    }
 }
